32b95a120e4af36261ed2ec08e3447622080bb15
Rain Gloom <raingloom42@gmail.com>
2015-07-28 17:01:01 +0200

    Made RebasePath more consistent

--- RebasePath.lua
+++ RebasePath.lua
@@ -12,25 +12,35 @@ return function ( opt )
   end
   
   local Path = opt.path or package.path
-  local Base = opt.Base or './'
-  local Target = opt.Target or '../Libs/'
+  local Base = opt.base or './'
+  local Target = opt.target or '../Libs/'
   local Separator = Path:sub ( 1, 1 )
   local BaseSafe = Escape ( Base )
   local SeparatorSafe = Escape ( Separator )
-
+  local PathPattern = '[^' .. SeparatorSafe ..']+'
+  
   local Rebaser
-  if opt.Append or opt.Append == nil then
+  local mode = opt.mode
+  if mode == 'append' then
     function Rebaser ( path )
       return path:gsub ( BaseSafe, Target ) .. Separator .. path
     end
-  else
+  elseif mode == 'replace' or mode == 'new' then
     function Rebaser ( path )
       return path:gsub ( BaseSafe, Target )
     end
   end
-
-  return Path:gsub (
-    '[^' .. SeparatorSafe ..']+',
-    Rebaser
-  )
+  
+  if mode == 'new' then
+    local newPaths, i = {}, 1
+    for path in Path:gmatch ( PathPattern ) do
+      newPaths [ i ], i = Rebaser ( path ), i + 1
+    end
+    return Separator .. table.concat ( newPaths, Separator )
+  else
+    return Path:gsub (
+      PathPattern,
+      Rebaser
+    )
+  end
 end
