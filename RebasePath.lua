09eb7a0883edf91ffda8a42cd6a8ba258f4c3c99
Rain Gloom <raingloom42@gmail.com>
2015-07-30 16:28:11 +0200

    More consistent path modification and better defaults handling

--- RebasePath.lua
+++ RebasePath.lua
@@ -1,4 +1,17 @@
 --TODO: add redirections to accomodate common repository paths
+--[[--
+  Returns a modified version of a pacakge path string
+  @param table|nil opt a table with options
+  options:
+    path: package path to use, defaults to pacakge.path
+    target: the new path component to use
+    base: search for this in paths, interpreted as a string pattern, defaults to './'
+    limit: limit number of substitutions to this number, defaults to 1
+    mode: defaults to 'replace'
+      append: mix the original and the rebased in place
+      replace: replace base in place
+      new: return only the modified paths
+]]
 return function ( opt )
   opt = opt or {}
   
@@ -13,21 +26,22 @@ return function ( opt )
   
   local Path = opt.path or package.path
   local Base = opt.base or './'
-  local Target = opt.target or '../Libs/'
+  local Target = assert ( opt.target, 'No target given' )
+  local Limit = opt.limit or 1
   local Separator = Path:sub ( 1, 1 )
   local BaseSafe = Escape ( Base )
   local SeparatorSafe = Escape ( Separator )
   local PathPattern = '[^' .. SeparatorSafe ..']+'
   
   local Rebaser
-  local mode = opt.mode
+  local mode = opt.mode or 'replace'
   if mode == 'append' then
     function Rebaser ( path )
-      return path:gsub ( BaseSafe, Target ) .. Separator .. path
+      return path:gsub ( BaseSafe, Target, Limit ) .. Separator .. path
     end
   elseif mode == 'replace' or mode == 'new' then
     function Rebaser ( path )
-      return path:gsub ( BaseSafe, Target )
+      return path:gsub ( BaseSafe, Target, Limit )
     end
   end
   
