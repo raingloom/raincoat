71de42bc89ed4ed5752bad2d7166e7e6ab613361
Rain Gloom <raingloom@users.noreply.github.com>
2015-10-06 01:35:37 +0200

    created a more generic version of Mt/Class.lua

---
+++ Mt/Callme.lua
@@ -0,0 +1,24 @@
+--[[-- Creates simple metatables with a __call field.
+  Made to reduce redundancy of metatables of the form:
+  `{
+    __call = function (self,...)
+      return self.somename (...)
+    end
+  }`
+  @param name a static function name (but any value will work)
+  @usage setmetatable ( class, require"raincoat.Mt.Callme""New" )
+]]
+local mtcache = {}
+
+return function ( name )
+  local mt = mtcache [ name ]
+  if not mt then
+    mt = {
+      __call = function ( self, ... )
+        return self [ name ] ( ... )
+      end
+    }
+    mtcache [ name ] = name
+  end
+  return mt
+end
