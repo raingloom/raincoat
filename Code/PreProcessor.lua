22679f69440c3b27082be419878cf40b39ef528f
raingloom <raingloom42@gmail.com>
2015-10-06 00:17:55 +0200

    fixed tabs in PP

--- Code/PreProcessor.lua
+++ Code/PreProcessor.lua
@@ -1,17 +1,18 @@
 local function generator ( buffer, env )
 	local code, i = { 'return coroutine.wrap ( function ( ... )' }, 2
 	local format = string.format
-	for line in buffer:lines () do
-    local prepScript = line:match '^%s*#(.*)'
+	local lines = buffer.lines
+	for line in (lines and lines(buffer) or buffer:gmatch"[^\n]+") do
+	local prepScript = line:match '^%s*#(.*)'
 		if prepScript then
 			code [ i ], code [ i + 1 ], i = prepScript, '\n', i + 2
 		else
 			local lastStart, lastEnd = 0, 0
-      for posStart, posEnd in line:gmatch '()$%b()()' do
-        code [ i ], i = format ( 'coroutine.yield %q;coroutine.yield ( %s );', line:sub ( lastEnd, posStart - 1), line:sub ( posStart + 2, posEnd - 2 ) ), i + 1
-        lastEnd = posEnd
-      end
-      code [ i ], i = format ( 'coroutine.yield ( %q )\ncoroutine.yield "\\n"\n', line:sub ( lastEnd ) ), i + 1
+	  for posStart, posEnd in line:gmatch '()$%b()()' do
+		code [ i ], i = format ( 'coroutine.yield %q;coroutine.yield ( %s );', line:sub ( lastEnd, posStart - 1), line:sub ( posStart + 2, posEnd - 2 ) ), i + 1
+		lastEnd = posEnd
+	  end
+	  code [ i ], i = format ( 'coroutine.yield ( %q )\ncoroutine.yield "\\n"\n', line:sub ( lastEnd ) ), i + 1
 		end
 	end
 	code [ i ] = 'end )'
