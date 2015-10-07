56e86323d2d1871b52dfc46430a9c565d23120f8
raingloom <raingloom42@gmail.com>
2015-10-07 19:19:31 +0200

    fixed stupid mistakes I made due to lack of coffee and/or sleep

--- Limits.lua
+++ Limits.lua
@@ -1,14 +1,19 @@
 local limits = {}
 
-do--find largest integer
-  local a, p = 1, 0
-  local n = 0
-  while p < a do
-    p = a
-    a = a << 1
-    n = n + 1
+do--find integer width
+  local w, n = 1, 1
+  while n > 0 do
+    n, w = n << 1, w + 1
   end
-  limits.maxint = p
+  limits.width = w
+end
+
+do--compute largest integer
+  local m = ~0
+  if m < 0 then--flip first bit to negate
+    m = m ~ 1 << (limits.width-1)
+  end
+  limits.maxint = m
 end
 
 return limits
