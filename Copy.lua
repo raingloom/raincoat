33bbd24a5dc6a286602b659c25a35195785a1245
Rain Gloom <raingloom@users.noreply.github.com>
2015-10-21 21:59:05 +0200

    Create Copy.lua

---
+++ Copy.lua
@@ -0,0 +1,33 @@
+local Copy = {}
+
+
+local unpack = unpack or table.unpack
+function Copy.array( t )
+    return {unpack( t )}
+end
+
+
+function Copy.pairs( t )
+    local ret = {}
+    for k, v in pairs( t ) do
+        ret[k] = v
+    end
+    return ret
+end
+
+
+functiony Copy.object( t )
+    return setmetatable(Copy.pairs( t ), getmetatable( t ))
+end
+
+
+function Copy.kvsplit( t )
+    local rk, rv, i = {}, {}, 1
+    for k, v in pairs( t ) do
+        rk[i], rv[i], i = k, v, i+1
+    end
+    return rk, rv
+end
+
+
+return Copy
