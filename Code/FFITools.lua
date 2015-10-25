7844d5cc0432ffc0651f89452338353cd56c33df
Rain Gloom <raingloom@users.noreply.github.com>
2015-10-25 12:24:14 +0100

    Create FFITools.lua

---
+++ Code/FFITools.lua
@@ -0,0 +1,22 @@
+---Helpers for writing optinally FFI optimized classes
+local FFITools = {}
+
+
+---Used for converting module paths to C-style names
+--@param name any string
+function FFITools.CSafeName( name )
+  --don't return number of matches
+  --TODO: more intelligent replacements?
+  return (name:gsub( '%W', '_' ))
+end
+
+
+---Converts a module name into a C-style type name
+-- @param name a module name
+-- @param stripped optionally strip the module string to only contain the last part
+function FFITools.typeName( name, stripped )
+  return FFITools.CSafeName( stripped and name:match '[^%.]+$' or name ) .. '_t'
+end
+
+
+return FFITools
