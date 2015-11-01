6bee938fbf2051a1f335b10005d8e6f26528adec
raingloom <raingloom42@gmail.com>
2015-11-01 18:03:22 +0100

    fixed failed attempt at optimization

--- Math/Factorize.lua
+++ Math/Factorize.lua
@@ -8,9 +8,8 @@ local Factorize = {}
 
 function Factorize.primeFactors( n )
 	local ret, i = {}, 1
-	local limit = math.floor( math.sqrt( n ) )
 	local prime, j = primes[ 1 ], 2
-	while prime <= limit do
+	while prime <= n do
 		if n%prime==0 then
 			ret[ i ], i = prime, i + 1
 		end
@@ -20,16 +19,22 @@ function Factorize.primeFactors( n )
 end
 
 
+--[[--
+	Factorizes an integer.
+	@param n the number to factorize
+	@param [primeFactors] if the sequence of prime factors for n have already been calculated, you can pass them here to avoid extra computation
+	@return a table with all prime divisors of n as keys and their respective multiplicities as values
+]]
 function Factorize.factorize( n, primeFactors )
 	local ret = {}
 	primeFactors = primeFactors or Factorize.primeFactors( n )
 	for i = 1, #primeFactors do
-		local prime, k = primeFactors[ i ], 0
+		local prime, multiplicity = primeFactors[ i ], 0
 		while n % prime == 0 do
-			k = k + 1
+			multiplicity = multiplicity + 1
 			n = n / prime
 		end
-		ret[ prime ] = k
+		ret[ prime ] = multiplicity
 	end
 	return ret
 end
