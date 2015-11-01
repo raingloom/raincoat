a6d3625acf878aad9710b53f7d36de3a5fab9755
raingloom <raingloom42@gmail.com>
2015-11-01 18:53:35 +0100

    added spec for multiplicity including factorizer

--- spec/Math_Factorize_spec.lua
+++ spec/Math_Factorize_spec.lua
@@ -8,4 +8,9 @@ describe( "Factorization module", function()
 		table.sort( factors )--the order they are returned in should not matter
 		assert.are.same( { 2, 5, 13 }, factors )
 	end)
+	it( "gives correct prime factors with multiplicities", function()
+		local factors = Factorize.factorize( 2^1*5^3*7^2 )
+		table.sort( factors )--the order they are returned in should not matter
+		assert.are.same( { [2] = 1, [5] = 3, [7] = 2 }, factors )
+	end)
 end)
