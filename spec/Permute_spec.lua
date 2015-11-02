48845dbc7900e1c1d24a709bc8352ada4b03a3e7
raingloom <raingloom42@gmail.com>
2015-11-02 15:47:45 +0100

    added spec for new permutation generator

--- spec/Permute_spec.lua
+++ spec/Permute_spec.lua
@@ -29,4 +29,39 @@ describe( "Permutation generators", function()
 			)
 		end)
 	end)
+	
+	describe( "limited integer sequence generator", function()
+		it( "returns all permutations exactly once", function()
+			local permutations = {}
+			for permutation in Permute.withinLimits{ 1, 1 } do
+				local stringHash = table.concat( permutation )
+				assert.is.falsy( permutations[ stringHash ] )
+				permutations[ stringHash ] = true
+			end
+			assert.are.same(
+				{
+					['00'] = true,
+					['01'] = true,
+					['10'] = true,
+					['11'] = true,
+				}, permutations
+			)
+		end)
+		it( "works with custom lower limit", function()
+			local permutations = {}
+			for permutation in Permute.withinLimits( { 2, 2 }, 1 ) do
+				local stringHash = table.concat( permutation )
+				assert.is.falsy( permutations[ stringHash ] )
+				permutations[ stringHash ] = true
+			end
+			assert.are.same(
+				{
+					['11'] = true,
+					['12'] = true,
+					['21'] = true,
+					['22'] = true,
+				}, permutations
+			)
+		end)
+	end)
 end)
