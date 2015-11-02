474dc5b9fce2879fbb7e4ab6b32b0979594b9474
raingloom <raingloom42@gmail.com>
2015-11-02 15:47:13 +0100

    added new permutation generator

--- Permute.lua
+++ Permute.lua
@@ -42,4 +42,42 @@ function Permute.unordered( t )
 end
 
 
+--[[--
+	Given an array of upper limits and an optional lower limit (default: 0), it generates sequences of integers where each element of the sequnce is <= then the corresponding higher limit and >= then the lower limit.
+	@param limits the sequence of upper limits
+	@param [low] lower limit
+	@return an iterator function
+	`low` must be `<=` then all elements of `limits`
+]]
+function Permute.withinLimits( limits, low )
+	return coroutine.wrap( function()
+		local ret = {}
+		local length = #limits
+		low = low or 0
+		local variations = 1
+
+		for i = 1, length do
+			ret[ i ] = low
+			variations = variations * (limits[ i ] - low + 1)
+			assert( limits[ i ] >= low, "Element larger than lower bound")
+		end
+
+		for i = 1, variations do
+			coroutine.yield( ret )
+			local remainder, i = true, 1
+			while remainder and i <= length do
+				local v = ret[ i ] + 1
+				if v > limits[ i ] then
+					v = low
+					remainder = true
+				else
+					remainder = false
+				end
+				ret[ i ], i = v, i + 1
+			end
+		end
+	end)
+end
+
+
 return Permute
