2c73203852b43dab9ec5a6d0d040959cd0df22f4
raingloom <raingloom42@gmail.com>
2015-11-02 21:28:28 +0100

    fixed indentation

--- Copy.lua
+++ Copy.lua
@@ -3,30 +3,30 @@ local Copy = {}
 
 local unpack = unpack or table.unpack
 function Copy.sequence( t )
-		return {unpack( t )}
+	return {unpack( t )}
 end
 
 
 function Copy.pairs( t )
-		local ret = {}
-		for k, v in pairs( t ) do
-				ret[k] = v
-		end
-		return ret
+	local ret = {}
+	for k, v in pairs( t ) do
+		ret[k] = v
+	end
+	return ret
 end
 
 
 function Copy.object( t )
-		return setmetatable( Copy.pairs( t ), getmetatable( t ) )
+	return setmetatable( Copy.pairs( t ), getmetatable( t ) )
 end
 
 
 function Copy.kvsplit( t )
-		local rk, rv, i = {}, {}, 1
-		for k, v in pairs( t ) do
-				rk[i], rv[i], i = k, v, i+1
-		end
-		return rk, rv
+	local rk, rv, i = {}, {}, 1
+	for k, v in pairs( t ) do
+		rk[i], rv[i], i = k, v, i+1
+	end
+	return rk, rv
 end
 
 
