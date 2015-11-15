b30d112110910b141499dadd7edf002890fb2179
raingloom <raingloom42@gmail.com>
2015-11-15 17:37:36 +0100

    removed init.lua

--- init.lua
+++
@@ -1,4 +0,0 @@
---TODO: Implement Penlight-style auto-loading, but without messing with _ENV or _G
-error 'Loader proxy not yet implemented, please require submodules explicitly'
-
-local ModulePath = (...)
