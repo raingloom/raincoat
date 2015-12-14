85ddd5ab33d3d8d2f223ae6c636206fae28dc419
raingloom <raingloom42@gmail.com>
2015-12-14 23:19:26 +0100

    modified Search.lua in some way
    idk it was a long time ago, okay?

--- Search.lua
+++ Search.lua
@@ -12,7 +12,7 @@ local Search = {}
 	@return value|number the value that was found or the key on which the search terminated
 	@return nil|number nothing if the search was successful, otherwise the value on which the search terminated
 ]]
-function Search.Binary ( orderedSequence, n )
+function Search.binary ( orderedSequence, n )
 	local len = #orderedSequence
 	local i = len
 	local step = len
@@ -39,13 +39,49 @@ end
 	@param [function] iterator an optional iterator function to use instead of pairs
 	@param ... pass these to the iterator function
 ]]
-function Search.Value ( tbl, value, iterator, ... )
+function Search.value ( tbl, value, iterator, ... )
 	for k, v in ( iterator or pairs ) ( tbl, ... ) do
 		if v == value then
 			return k, v
 		end
 	end
 end
+
+
+--[[--
+	Runs an advanced recursive search on a table.
+	@tparam table tbl
+	@tparam fun a function (or something callable)
+	@tparam function iter the table iterator function, in case you want to use stg other than require
+	@return the discovered value or nothing if it was not found
+	@return the path that led to the value
+]]
+function Search.deepFilter( tbl, fun, iter, _path, _guard )
+	iter = iter or pairs
+	_guard = _guard or {}
+	_path = _path or {}
+	_guard[tbl] = true
+	local tl = {}
+	for k, v in iter( tbl ) do
+		_path[#_path+1]=k
+		if fun(v,_path) then
+			return v, _path
+		end
+		_path[#_path]=nil
+		if type( v ) == 'table' then
+			tl[#tl+1]=k
+		end
+	end
+	_path[#_path]=nil
+	for _, k in ipairs( tl ) do
+		local t = tbl[k]
+		if not _guard[t] then
+			_path[#_path+1]=k
+			return Search.deepFilter( t, fun, iter, _path, _guard )
+		end
+	end
+end
+
 
 
 return Search
