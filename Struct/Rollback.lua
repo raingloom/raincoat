b5efb96665a30824fc6b680528442080504c1013
raingloom <raingloom42@gmail.com>
2015-12-21 20:30:46 +0100

    versioned table

---
+++ Struct/Rollback.lua
@@ -0,0 +1,49 @@
+local Rlb = {}
+Rlb.__index = Rlb
+
+
+local function newstate( seed )
+	return setmetatable(seed or {}, Rlb)
+end
+
+
+local function newindex( rlb, key, value )
+	rlb.__index[key] = value
+end
+
+
+local function new ( seed )
+	seed = newstate( seed )
+	local ret = { __states = { seed }, __stateid = 1 }
+	ret.__index = seed
+	ret.__newindex = newindex
+	return setmetatable( ret, ret )
+end
+
+
+function Rlb:__save()
+	local nstate = {}
+	nstate.__index = self.__index
+	setmetatable( nstate, nstate )
+	self.__index = nstate
+	local stateid = self.__stateid + 1
+	self.__stateid = stateid
+	self.__states[ stateid ] = nstate
+	return stateid
+end
+
+
+function Rlb:__rollback( erase )
+	local stateid = self.__stateid - 1
+	if stateid > 0 then
+		if erase then
+			self.__states[ stateid + 1 ] = nil
+		end
+		self.__index = self.__states[stateid]
+		return stateid
+	end
+	return nil, "Can't roll back beyond initial state."
+end
+
+
+return new
